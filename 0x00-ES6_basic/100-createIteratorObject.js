86bbc4aaaea6ab4b9e24dcec4e3fd9e874d8e6f8
belal <belalismail252@gmail.com>
2024-07-23 02:10:33 +0300

    Es6 taasks

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const value of Object.values(report.allEmployees)) {
+    allEmployees = [...allEmployees, ...value];
+  }
+
+  return allEmployees;
+}
