e6bbbf8b7d115a042e71530e7cf9e0b901c6f244
belal <belalismail252@gmail.com>
2024-07-24 23:29:13 +0300

    simple error

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  const result = [];
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]).then((results) => {
     results.map(({ status, value, reason }) => (
-      results.push({
+      result.push({
         status,
         value: status === 'rejected' ? reason.toString() : value,
       })
