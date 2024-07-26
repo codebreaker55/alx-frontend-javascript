e5ccd79e84a0f427ba61428b53d287a33eff4051
belal <belalismail252@gmail.com>
2024-07-26 23:27:59 +0300

    new try

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,18 +1,14 @@
 import Car from './10-car';
 
+// impleminting EVCar class that extends from the car class
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  // using eslint-disable-next-line the class-methods-use-this
+  // eslint-disable-next-line class-methods-use-this
   cloneCar() {
-    return new EVCar(
-      this.brand,
-      this.motor,
-      this.color,
-      this._range,
-    );
+    return new Car();
   }
 }
