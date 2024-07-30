0b76b50c7c5b2704aa0b4d2c9e57f59ce439c803
belal <belalismail252@gmail.com>
2024-07-30 11:37:53 +0300

    data manipulaation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(studentsList, city, newGrades) 
   // using filter to get students in the specified city
   const studentCity = studentsList.filter((student) => student.location === city);
 
-const updateStudents = studentCity.map((student) => {
+  const updateStudents = studentCity.map((student) => {
     // find the student grade for the student in newGrades array
     const studentGrade = newGrades.find((grade) => grade.studentId === student.id);
 
