b8b39d2608ff80bb8bec0c6141b215188ac6b34c
belal <belalismail252@gmail.com>
2024-07-31 15:04:49 +0300

    typescripts

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,51 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  contract: boolean;
+  [propName: string]: any;
+}
+
+// Write an interface named Directors that extends Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Write an interface for the function named printTeacherFunction
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Write a Class named StudentClass
+const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+};
+
+export { printTeacher, StudentClass };
